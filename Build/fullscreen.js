66176941ac546fcb706d2c6b2c09ee10318947a1
HamzaAziz0320 <118163884+HamzaAziz0320@users.noreply.github.com>
2024-05-21 17:32:16 +0500

    Build Version 32

---
+++ Build/fullscreen.js
@@ -0,0 +1,9 @@
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(err => {
+            console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+}
